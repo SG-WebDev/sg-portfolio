02c791d892e8690bb529c3efd73114adc0174255
SG-WebDev <sgrobelny.contact@gmail.com>
2019-09-01 22:28:58 +0200

    navigation desktop styles

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,18 @@
 const navItems = document.querySelectorAll('.nav__Item');
 navItems.forEach( navItem => {
     navItem.addEventListener('click', function (e) {
-        e.preventDefault();
         let target = this.getAttribute('data-target');
+        navItems.forEach( elem => {
+           elem.classList.remove('nav__Item--active');
+        });
+        this.classList.add('nav__Item--active');
+        window.location.href = `${target}#`;
         document.querySelector(`${target}`).scrollIntoView({
             behavior: 'smooth'
         });
+        return false;
     }, false);
+});
+window.addEventListener('load', (event) => {
+    window.location.href = '#';
 });
